f0912b8af78e1817d88a4eed3994c917e99b0d25
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2021-03-29 14:10:47 -0500

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.rst', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bipartitepandas',
+    version='0.0.11',
+    author='Thibaut Lamadon',
+    author_email='thibaut.lamadon@gmail.com',
+    description='Python tools for bipartite labor data',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/tlamadon/bipartitepandas',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'numpy_groupies',
+        'scipy',
+        'scikit-learn',
+        'qpsolver',
+        'networkx',
+        'tqdm',
+      ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
