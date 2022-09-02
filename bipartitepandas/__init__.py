9b0ef81befa975cf9b2ccbbcc92c48d90526bc76
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2022-09-02 18:01:39 -0500

    Move CHK into PyTwoWay

--- bipartitepandas/__init__.py
+++ bipartitepandas/__init__.py
@@ -4,7 +4,7 @@ from .measures import measures
 from .grouping import grouping
 from .bipartitebase import clean_params, cluster_params, BipartiteBase
 from .bipartitelongbase import BipartiteLongBase
-from .bipartitelong import plot_extended_eventstudy_params, BipartiteLong
+from .bipartitelong import BipartiteLong
 from .bipartitelongcollapsed import BipartiteLongCollapsed
 from .bipartiteeventstudybase import BipartiteEventStudyBase
 from .bipartiteeventstudy import BipartiteEventStudy
