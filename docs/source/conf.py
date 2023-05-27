02d511fc6df8124165b587e30e2b21657648a3b3
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2023-05-26 22:51:46 -0500

    Pip version 1.1.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.4'
+release = '1.1.5'
 
 
 # -- General configuration ---------------------------------------------------
